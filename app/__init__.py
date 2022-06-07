0732d7313cf6caaa0eaa7bab13d6a61f7b90d6da
Álex Rody <hey@alexrody.com>
2022-06-06 22:55:28 -0500

    Add navigation bar in darker blue with new logo, fix #10

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="Álex Rody", url=os.getenv("URL"))
+
+@app.route('/experience')
+def experience():
+    return "Experience"
+
+@app.route('/education')
+def education():
+    return "Education"
+
+@app.route('/hobbies')
+def hobbies():
+    return "Hobbies"
