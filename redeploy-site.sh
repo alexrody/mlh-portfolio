aaccfd0e91f9340428ff71bd57d22784840de22f
Álex Rody <hey@alexrody.com>
2022-06-26 15:15:10 -0500

    Update redeploy script and add service file

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-tmux kill-server
 # cd mlh-portfolio
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d -s flaskapp
-tmux send-keys 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0' C-m
+systemctl restart myportfolio
