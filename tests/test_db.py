cd801a1b1857228c33d047afa3237f78151a0752
Javier Solis <javsolis@mit.edu>
2022-07-04 21:20:20 -0500

    Added 2  database tests

---
+++ tests/test_db.py
@@ -0,0 +1,36 @@
+#!/user/bin/env python3
+# Authors: MLH, Javier Solis
+
+import unittest
+from peewee import *
+from app import TimelinePost
+
+MODELS = [TimelinePost]
+
+test_db = SqliteDatabase(':memory:') # switching to an in-memory DB
+
+class TestTimelinePost(unittest.TestCase):
+    def setUp(self):
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+        test_db.connect()
+        test_db.create_tables(MODELS)
+        
+    def tearDown(self):
+        test_db.drop_tables(MODELS)
+        test_db.close()
+    
+    def test_timeline_post(self):
+        first_post = TimelinePost.create(
+            name="John Doe",
+            email="john@example.com",
+            content="Hello world, I\'m John!"
+        )
+
+        assert first_post.id == 1
+
+        second_post = TimelinePost.create(name="Jane Doe",
+            email="Jane@example.com",
+            content="Hello world, I\'m Jane!")
+        assert second_post.id == 2
+        
+        #TODO: check the post list for these two
